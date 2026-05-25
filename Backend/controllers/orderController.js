b0f89746432d2823c126d422385152c9243e6481
KalangiGeetaneha <geetanehakalangi@gmail.com>
2026-05-25 22:34:10 +0530

    Remove trailing slash from frontend URL

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -12,7 +12,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
 
-    const frontend_url = "https://food-delivery-frontend-e2lj.onrender.com/";
+    const frontend_url = "https://food-delivery-frontend-e2lj.onrender.com";
 
     try {
 
