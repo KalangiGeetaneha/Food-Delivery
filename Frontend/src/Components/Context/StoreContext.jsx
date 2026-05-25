d21c68e3c65652748aa2f0036dfa5820b28297e2
KalangiGeetaneha <geetanehakalangi@gmail.com>
2026-05-25 21:06:58 +0530

    Change API URL to production endpoint

--- Frontend/src/Components/Context/StoreContext.jsx
+++ Frontend/src/Components/Context/StoreContext.jsx
@@ -7,7 +7,7 @@ const StoreContextProvider = (props) => {
 
   const [cartItems, setCartItems] = useState({});
 
-  const url = "http://localhost:8000";
+  const url = "https://food-delivery-backend-gdc4.onrender.com";
 
   const [token, setToken] = useState("");
 
