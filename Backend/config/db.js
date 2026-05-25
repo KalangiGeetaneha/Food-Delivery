5a49e878537478893d36369dc97a6f7c4a4924ea
Geeta Neha <geetanehakalangi@gmail.com>
2026-05-25 21:59:51 +0530

    fixed mongodb atlas and image loading

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose  from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/food-del');
+    await mongoose.connect('mongodb+srv://geetanehakalangi:Girija1986@cluster0.khwxup7.mongodb.net/food-del');
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error);
